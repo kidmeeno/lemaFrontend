4f0ede3c1f94b615d3fb4f92aa8d7778755db8e5
kidmeeno <arthurbosah@yahoo.com>
2025-07-24 12:24:08 +0100

    Add network check for when offline

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import { Route, Routes } from 'react-router-dom';
 import UsersPage from './pages/users/UsersPage';
 import PostsPage from './pages/posts/PostPage';
 import { Bounce, ToastContainer } from 'react-toastify';
+import useNetworkStatus from './hooks/useNetworkStatus';
 
 export default function App() {
+  const isOnline = useNetworkStatus();
+
   return (
     <>
+      {!isOnline && (
+        <div className='bg-red-100 text-red-800 text-center p-3'>
+          🚫 You're offline. Please check your internet connection.
+        </div>
+      )}
       <Routes>
         <Route path='/' element={<UsersPage />} />
         <Route path='/:id' element={<PostsPage />} />
