bea76484c9a13fb432a6dba481d6875850d44fe5
kidmeeno <arthurbosah@yahoo.com>
2025-07-24 07:57:11 +0100

    change post input directry

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import UsersPage from "./pages/users/UsersPage";
-import PostsPage from "./pages/posts/postPage";
+import PostsPage from "./pages/posts/PostPage";
 
 export default function App() {
   return (
